5a6880ce65f523c687add100177cc3aa5a799887
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-10-24 22:24:59 -0400

    chore: seed with questions

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,19 +12,29 @@
 
 alias AskStream.{Repo, QA}
 
-# Create fake sessions
+# Create fake sessions with questions
 fake_sessions =
   for _ <- 1..20 do
     datetime =
       Faker.DateTime.between(~D"2023-01-01", ~D"2023-10-01") |> DateTime.truncate(:second)
 
-    %{
+    Repo.insert!(%QA.Session{
       name: "Q/A #{DateTime.to_date(datetime)}",
       inserted_at: datetime,
-      updated_at: datetime
-    }
+      updated_at: datetime,
+      questions:
+        for _ <- 1..10 do
+          question_datetime =
+            datetime
+            |> Faker.DateTime.between(DateTime.add(datetime, 2, :hour))
+            |> DateTime.truncate(:second)
+
+          %QA.Question{
+            question: Faker.Lorem.sentence(),
+            author_username: Faker.Internet.user_name(),
+            inserted_at: question_datetime,
+            updated_at: question_datetime
+          }
+        end
+    })
   end
-
-dbg(fake_sessions)
-
-Repo.insert_all(QA.Session, fake_sessions)
