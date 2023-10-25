2cf7d12d344b250128f1658b880d94b95ffa2dfd
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-10-24 21:00:12 -0400

    chore: install faker and seed the db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias AskStream.{Repo, QA}
+
+# Create fake sessions
+fake_sessions =
+  for _ <- 1..20 do
+    datetime =
+      Faker.DateTime.between(~D"2023-01-01", ~D"2023-10-01") |> DateTime.truncate(:second)
+
+    %{
+      name: "Q/A #{DateTime.to_date(datetime)}",
+      inserted_at: datetime,
+      updated_at: datetime
+    }
+  end
+
+dbg(fake_sessions)
+
+Repo.insert_all(QA.Session, fake_sessions)
